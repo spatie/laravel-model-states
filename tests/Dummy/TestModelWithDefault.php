8135707973557d1ea8856bda3137a83bbe5860f5
Brent Roose <brent@spatie.be>
2020-08-21 13:22:17 +0200

    Wip

--- tests/Dummy/TestModelWithDefault.php
+++ tests/Dummy/TestModelWithDefault.php
@@ -2,34 +2,19 @@
 
 namespace Spatie\ModelStates\Tests\Dummy;
 
-use Illuminate\Database\Eloquent\Model;
-use Spatie\ModelStates\HasStates;
 use Spatie\ModelStates\Tests\Dummy\States\ModelState;
 use Spatie\ModelStates\Tests\Dummy\States\StateA;
 use Spatie\ModelStates\Tests\Dummy\States\StateB;
 use Spatie\ModelStates\Tests\Dummy\States\StateC;
 
-/**
- * @method static self create(array $extra = [])
- * @method static self find(int $id)
- * @property ModelState state
- */
-class TestModel extends Model
+class TestModelWithDefault extends TestModel
 {
-    protected $guarded = [];
-
-    use HasStates;
-
-    public function getTable()
-    {
-        return 'test_models';
-    }
-
     public function registerStates(): void
     {
         $this
             ->addState('state', ModelState::class)
             ->allowTransition(StateA::class, StateB::class)
-            ->allowTransition(StateA::class, StateC::class);
+            ->allowTransition(StateA::class, StateC::class)
+            ->default(StateA::class);
     }
 }
