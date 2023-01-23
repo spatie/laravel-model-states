471270b76280b3a3d42f520f94a78d4b8b69c161
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2023-01-23 16:35:58 +0100

    Add missing RegisterState Attribute

--- tests/Dummy/AttributeState/AttributeState.php
+++ tests/Dummy/AttributeState/AttributeState.php
@@ -4,12 +4,19 @@ namespace Spatie\ModelStates\Tests\Dummy\AttributeState;
 
 use Spatie\ModelStates\Attributes\AllowTransition;
 use Spatie\ModelStates\Attributes\DefaultState;
+use Spatie\ModelStates\Attributes\RegisterState;
 use Spatie\ModelStates\State;
+use Spatie\ModelStates\Tests\Dummy\AttributeState\AnotherDirectory\AttributeStateC;
+use Spatie\ModelStates\Tests\Dummy\AttributeState\AnotherDirectory\AttributeStateD;
+use Spatie\ModelStates\Tests\Dummy\AttributeState\AnotherDirectory\AttributeStateE;
 
 #[
     AllowTransition(AttributeStateA::class, AttributeStateB::class, AttributeStateTransition::class),
     AllowTransition(AttributeStateB::class, AttributeStateA::class),
+    AllowTransition(AttributeStateA::class, AttributeStateC::class),
     DefaultState(AttributeStateA::class),
+	RegisterState(AttributeStateC::class),
+	RegisterState([AttributeStateD::class, AttributeStateE::class]),
 ]
 abstract class AttributeState extends State
 {
