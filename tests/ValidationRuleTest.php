4393203c2ec8a2815d8855d67ea8b381b8560914
Jure Knezovic <jknezovic@byte5.de>
2023-05-09 01:07:51 +0200

    feat: Tests for new rule

--- tests/ValidationRuleTest.php
+++ tests/ValidationRuleTest.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Validator;
 use Spatie\ModelStates\Tests\Dummy\ModelStates\ModelState;
 use Spatie\ModelStates\Tests\Dummy\ModelStates\StateA;
+use Spatie\ModelStates\Tests\Dummy\ModelStates\StateB;
+use Spatie\ModelStates\Tests\Dummy\ModelStates\StateC;
+use Spatie\ModelStates\Tests\Dummy\ModelStates\StateD;
+use Spatie\ModelStates\Tests\Dummy\TestModel;
 use Spatie\ModelStates\Validation\ValidStateRule;
+use Spatie\ModelStates\Validation\ValidStateTransitionRule;
 
-it('test validation', function () {
+it('test state validation', function () {
     $rule = new ValidStateRule(ModelState::class);
 
     expect(! Validator::make(
@@ -19,7 +24,7 @@ it('test validation', function () {
     )->fails())->toBeFalse();
 });
 
-it('nullable validation', function () {
+it('test nullable state validation', function () {
     $rule = (new ValidStateRule(ModelState::class))->required();
 
     expect(Validator::make(
@@ -29,6 +34,56 @@ it('nullable validation', function () {
 
     $rule = (new ValidStateRule(ModelState::class))->nullable();
 
+    expect(Validator::make(
+        ['state' => null],
+        ['state' => $rule]
+    )->fails())->toBeFalse();
+});
+
+
+it('test transition validation', function () {
+    $modelA = TestModel::create([
+        'state' => StateA::class,
+    ]);
+    $rule = new ValidStateTransitionRule(ModelState::class,$modelA);
+
+    expect(Validator::make(
+        ['state' => StateB::getMorphClass()],
+        ['state' => $rule]
+    )->passes())->toBeTrue();
+
+    expect(Validator::make(
+        ['state' => StateC::getMorphClass()],
+        ['state' => $rule]
+    )->passes())->toBeTrue();
+
+
+    $modelB= TestModel::create([
+        'state' => StateB::class,
+    ]);
+    $rule = new ValidStateTransitionRule(ModelState::class,$modelB);
+
+    expect(Validator::make(
+        ['state' => StateA::getMorphClass()],
+        ['state' => $rule]
+    )->fails())->toBeTrue()
+        ->and(Validator::make(
+            ['state' => StateD::getMorphClass()],
+            ['state' => $rule]
+        )->fails())->toBeTrue();
+});
+
+it('test nullable transition validation', function () {
+    $modelA = TestModel::create([
+        'state' => StateA::class,
+    ]);
+    $rule = (new ValidStateTransitionRule(ModelState::class,$modelA))->required();
+    expect(Validator::make(
+        ['state' => null],
+        ['state' => $rule]
+    )->fails())->toBeTrue();
+
+    $rule = (new ValidStateTransitionRule(ModelState::class,$modelA))->nullable();
     expect(Validator::make(
         ['state' => null],
         ['state' => $rule]
