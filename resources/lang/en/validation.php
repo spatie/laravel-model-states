e48e3b84ee37f68232f3bd777844f4c384eeb1a8
Jure Knezovic <jknezovic@byte5.de>
2023-05-09 01:07:43 +0200

    fix: Formating

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -2,6 +2,6 @@
 
 return [
     'invalid' => 'This value is invalid',
-    'transition_not_allowed'=>'Requested state transition is not allowed.',
-    'transition_not_allowed_with_allowed'=>'Requested state transition is not allowed. Allowed transitions are: :transitions',
+    'transition_not_allowed' => 'Requested state transition is not allowed.',
+    'transition_not_allowed_with_allowed' => 'Requested state transition is not allowed. Allowed transitions are: :transitions'
 ];
